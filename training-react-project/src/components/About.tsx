23f7eef2a4ea851d1e5ed26e4ea37b7994410f5e
Poojan Pandya <pandyapoojan2000@gmail.com>
2022-01-03 10:30:02 +0530

    Initial commit

---
+++ training-react-project/src/components/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+           <h1>This is About Page.</h1> 
+           <p>This page is located on '/about' route.</p>
+        </div>
+    )
+}
+
+export default About
---
+++ training-react-project/src/components/Users.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Users = () => {
+    return (
+        <div>
+            <h1>This is Users Page</h1>
+            <p>This page is located on '/users' route.</p>
+        </div>
+    )
+}
+
+export default Users
